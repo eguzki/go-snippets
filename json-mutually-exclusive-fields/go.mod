74b347d5af83ad39809270116f8a2390ad473821
Eguzki Astiz Lezaun <eastizle@redhat.com>
2024-09-20 18:00:05 +0200

    json-mutually-exclusive-fields v2

---
+++ json-mutually-exclusive-fields/go.mod
@@ -0,0 +1,5 @@
+module example.com/m
+
+go 1.22.5
+
+require github.com/google/go-cmp v0.6.0
--- json-mutually-exclusive-fields/main.go
+++ json-mutually-exclusive-fields/main.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 type SelectorSpec struct {
@@ -25,7 +27,7 @@ type Selector struct {
 }
 
 type DataType struct {
-	Value interface{}
+	Value interface{} `json:,inline`
 }
 
 func (d *DataType) UnmarshalJSON(data []byte) error {
@@ -43,7 +45,7 @@ func (d *DataType) UnmarshalJSON(data []byte) error {
 		//fmt.Printf("unmarshalling %+v\n", types[idx])
 		dec := json.NewDecoder(bytes.NewReader(data))
 		dec.DisallowUnknownFields() // Force errors
-		err = dec.Decode(&types[idx])
+		err = dec.Decode(types[idx])
 		if err == nil {
 			d.Value = types[idx]
 			return nil
@@ -136,5 +138,66 @@ func main() {
 		var conf Conf
 		err := json.Unmarshal([]byte(test.jsonStr), &conf)
 		fmt.Printf("testing %s: %t, err: %v\n", test.name, test.valid == (err == nil), err)
+
+		if err == nil && test.valid {
+			for idx := range conf.Data {
+				switch val := conf.Data[idx].Value.(type) {
+				case *Static:
+					fmt.Printf("testing %s: is static key: %s\n", test.name, val.Static.Key)
+				case *Selector:
+					fmt.Printf("testing %s: is selector key: %s\n", test.name, val.Selector.Selector)
+				default:
+					panic("should not happen")
+				}
+			}
+		}
+
+		serialized, err := json.Marshal(conf)
+		fmt.Printf("marshal error: %v\n", err)
+		fmt.Println(string(serialized))
+
 	}
+
+	fmt.Println("== compare method")
+	config := Conf{
+		Name: "validJson",
+		Data: []DataType{
+			{Value: &Static{Static: StaticSpec{Key: "keyA"}}},
+			{Value: &Selector{Selector: SelectorSpec{Selector: "selectorA"}}},
+			{Value: &Static{Static: StaticSpec{Key: "keyB"}}},
+			{Value: &Selector{Selector: SelectorSpec{Selector: "selectorB"}}},
+		},
+	}
+
+	var unmarshaledConfig Conf
+	configMarshaled := []byte(`
+{
+	"name": "validJson",
+	"data": [
+		{
+			"static": { "key": "keyA" }
+		},
+		{
+			"selector": { "selector": "selectorA" }
+		},
+		{
+			"static": { "key": "keyB" }
+		},
+		{
+			"selector": { "selector": "selectorB" }
+		}
+	]
+}
+`)
+	if err := json.Unmarshal(configMarshaled, &unmarshaledConfig); err != nil {
+		panic(err)
+	}
+
+	if !cmp.Equal(unmarshaledConfig, config) {
+		diff := cmp.Diff(unmarshaledConfig, config)
+		fmt.Println("Diff")
+		fmt.Println(diff)
+	}
+
+	fmt.Println("no diff 🎉")
 }
